25d8430583d1137acfa86309918db5760459ed31
Wendryo <wendryo.sales@gmail.com>
2023-07-07 22:15:14 -0300

    feat: interfaces for Events

--- src/interfaces/repository/IEventRepository.ts
+++ src/interfaces/repository/IEventRepository.ts
@@ -1,7 +1,9 @@
 import { Events } from '@prisma/client';
+import { ICreateEvent } from '../../types/events';
 
 interface IEventRepository {
-  getEvents():Promise<Events[]>
+  getEvents(): Promise<Events[]>
+  createEvent(body: ICreateEvent): Promise<Events>
 }
 
 export default IEventRepository;
